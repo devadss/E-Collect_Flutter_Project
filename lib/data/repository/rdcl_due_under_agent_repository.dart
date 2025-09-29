6d5e09f752b7664640c8672546a331a079778a87
adss adss <devopsadss@gmail.com>
2025-09-29 16:22:21 +0530

    latest changes

--- lib/data/repository/rdcl_due_under_agent_repository.dart
+++ lib/data/repository/rdcl_due_under_agent_repository.dart
@@ -2,48 +2,49 @@ import 'dart:convert';
 import 'package:collection_qr_flutter/core/general.dart';
 import 'package:collection_qr_flutter/domain/model/due_model/rdcl_due_under_agent_model.dart';
 import 'package:dartz/dartz.dart';
-import 'package:http/http.dart'as http;
+import 'package:http/http.dart' as http;
 import '../../domain/interface/rdcl_due_under_agent_interface.dart';
 import '../storage/shared_pref_helper.dart';
 
-class RdclDueUnderAgentRepo implements RdclDueUnderAgentModelInterface{
+class RdclDueUnderAgentRepo implements RdclDueUnderAgentModelInterface {
   Future<String> loadVendorUrl() async {
     return await SharedPref().getDueListUrl();
-
   }
 
   @override
-  Future<Either<String, RdclDueUnderAgentModel>> getRdclDueList(String agentId,
-      String branchCode, String accNo, int pageNo, int pageSize,String custName) async {
+  Future<Either<String, RdclDueUnderAgentModel>> getRdclDueList(
+      String agentId,
+      String branchCode,
+      String accNo,
+      int pageNo,
+      int pageSize,
+      String custName) async {
     print("inside getRdclDueList");
     final vendorUrl = await loadVendorUrl();
     print("vendorUrl = $vendorUrl");
-   final uri = Uri.parse("$vendorUrl?agent_id=$agentId&br_code=$branchCode&acc_no=$accNo&PageNumber=$pageNo&PageSize=$pageSize&CustName=$custName");
+    final uri = Uri.parse(
+        "$vendorUrl?agent_id=$agentId&br_code=$branchCode&acc_no=$accNo&PageNumber=$pageNo&PageSize=$pageSize&CustName=$custName");
     print("uri = $uri");
-    try{
-      if(checkInternetConnection() == true){
+    try {
+      if (checkInternetConnection() == true) {
         print("Network connection success");
-        final request = await  http.get(uri);
+        final request = await http.get(uri);
         print(request.statusCode);
         print("$vendorUrl?agent_id=$agentId");
         print("GetRdclDuesList ${request.body}");
-        if(request.statusCode == 200){
-          return Right(RdclDueUnderAgentModel.fromJson(jsonDecode(request.body)));
-
-        }else{
+        if (request.statusCode == 200) {
+          return Right(
+              RdclDueUnderAgentModel.fromJson(jsonDecode(request.body)));
+        } else {
           return Left(request.body);
         }
-      }else{
+      } else {
         return const Left("Check internet connection");
       }
-    }catch(e){
+    } catch (e) {
       return const Left("Unable to fetch Due Under Agent");
     }
-
-
   }
-  
 }
-
 
 //final uri = Uri.parse("https://doorstepclientuat.digicob.in/GetRdclDuesListunderAgent?agent_id=$agentId&br_code=$branchCode&acc_no=$accNo&PageNumber=$pageNo&PageSize=$pageSize&CustName=$custName");
