48f193e4fc2f4e3bf58f7e66b13846714c8b2d82
devopsadss <devopsadss@gmail.com>
2025-11-12 11:33:03 +0530

    chnages

--- lib/presentation/app/bottom_nav_bar_page.dart
+++ lib/presentation/app/bottom_nav_bar_page.dart
@@ -13,6 +13,7 @@ import '../groups/homepage/group_home_page.dart';
 import '../home/home_page.dart';
 import '../loan/loan_home_page.dart';
 import '../profile/profile_home_page.dart';
+import '../test_page.dart';
 import '../trancstion/payment_link_home_page.dart';
 
 class BottomNavScreen extends StatefulWidget {
@@ -59,8 +60,8 @@ class _BottomNavScreenState extends State<BottomNavScreen> {
        // return const LoanHomePage();
         return const LoanList();
       case 2:
-        return const ProfileHomePage();
-      // return const TestPage();
+      //  return const ProfileHomePage();
+       return const TestProfilePage();
       default:
         return  HomePage(userType: loggedInUserTPYE.toString(),);
     }
--- lib/presentation/test_page.dart
+++ lib/presentation/test_page.dart
@@ -1,174 +1,312 @@
 import 'package:collection_qr_flutter/core/colors.dart';
 import 'package:flutter/material.dart';
 
-class TestPage extends StatefulWidget {
-  const TestPage({super.key});
+import '../data/storage/shared_pref_helper.dart';
+
+class TestProfilePage extends StatefulWidget {
+  const TestProfilePage({super.key});
 
   @override
-  State<TestPage> createState() => _TestPageState();
+  State<TestProfilePage> createState() => _TestProfilePageState();
 }
 
-class _TestPageState extends State<TestPage> {
-  final List<String> itemList = [
-    "Item 1",
-    "Item 2",
-    "Item 3",
-    "Item 4",
-    "Item 5"
-  ];
-  String? selectedItem;
+class _TestProfilePageState extends State<TestProfilePage> {
+  String name = "Unknown User";
+  String mobNum = "No Number";
+  final ScrollController _scrollController = ScrollController();
+
+  Future<void> loadSharedData() async {
+    String? username = await SharedPref.shared.getSubAgentName();
+    String? usermobNum = await SharedPref.shared.getSubAgentMobNum();
+
+    if (mounted) {
+      setState(() {
+        name = username ?? "Unknown User";
+        mobNum = usermobNum ?? "No Number";
+      });
+    }
+  }
+
+  void doAutoScroll(){
+    _scrollController.animateTo(_scrollController.position.maxScrollExtent, duration: Duration(milliseconds: 500), curve: Curves.linear);
+  }
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_){
+      doAutoScroll();
+    });
+    loadSharedData();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        const SizedBox(height: 50),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                width: 120,
-                height: 120, // Optional: give height to match icon size
-                decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                        offset: const Offset(5, 2),
-                        color: Colors.deepPurple.shade900,
-                        spreadRadius: 2,
-                        blurRadius: 5)
-                  ],
-                  color: home2,
-                  borderRadius: BorderRadius.circular(360),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        controller: _scrollController,
+        child: Column(
+          children: [
+            Stack(
+              clipBehavior: Clip.none,
+              children: [
+                Image.asset(
+                  "assets/images/new_profile_bg.png",
+                  fit: BoxFit.fill,
+                  width: double.infinity,
+                  height: 330,
                 ),
-                child: const Center(
-                  child: Icon(
-                    Icons.person,
-                    size: 80,
-                    color: home1,
-                  ),
-                ),
-              ),
-              const SizedBox(width: 20),
-              // Add spacing between avatar and text column
-              const Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("Abhilash K R",
-                      style:
-                          TextStyle(fontSize: 22, fontWeight: FontWeight.w700)),
-                  SizedBox(height: 4),
-                  SizedBox(
-                    width: 200,
-                    child: Divider(color: Colors.black, thickness: 1),
-                  ),
-                  Text("abhilassurendran@gmail.com",
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.w700)),
-                  SizedBox(height: 4),
-                  SizedBox(
-                    width: 200,
-                    child: Divider(color: home2, thickness: 1),
-                  ),
-                  Text("+91 9898091250", style: TextStyle(fontSize: 16)),
-                ],
-              ),
-            ],
-          ),
-        ),
-        ListWidgetMethod(
-          itms: itemList,
-          selectedItem: selectedItem.toString(),
-        )
-      ],
-    ));
-  }
-}
-
-void showDialogNotification(
-    BuildContext context, int no, String itemSelction, List<String> itms) {
-  showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text("Selection"),
-          content: DropdownMenu<String>(
-            label: const Text("Select any"),
-            initialSelection: itemSelction,
-            dropdownMenuEntries: itms
-                .map((itms) => DropdownMenuEntry(value: itms, label: itms))
-                .toList(),
-          ),
-          actions: [
-            TextButton(onPressed: () {}, child: const Text("d sd sd sd"))
-          ],
-        );
-      });
-}
-
-class ListWidgetMethod extends StatelessWidget {
-  final String selectedItem;
-  final List<String> itms;
-
-  const ListWidgetMethod(
-      {super.key, required this.selectedItem, required this.itms});
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-        child: ListView.builder(
-            itemCount: 100,
-            itemBuilder: (BuildContext context, int index) {
-              return Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: const [
-                      BoxShadow(
-                          offset: Offset(3, 1),
-                          color: Colors.white,
-                          blurRadius: 3,
-                          spreadRadius: 5)
-                    ]),
-                height: 70,
-                child: GestureDetector(
-                  onTap: () {
-                    showDialogNotification(context, index, selectedItem, itms);
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: Card(
-                      shadowColor: Colors.black12,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadiusGeometry.circular(10)),
-                      elevation: 5,
-                      child: Center(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const Icon(
-                                Icons.ac_unit,
-                                color: home1,
-                              ),
-                              SizedBox(
-                                  width: 350,
-                                  child: Text("Content No is : $index")),
-                              const Icon(Icons.arrow_forward_ios)
-                            ],
-                          ),
-                        ),
+                Positioned(
+                    top: 200,
+                    left: 0,
+                    right: 0,
+                    child: Center(
+                      child: Image.asset(
+                        "assets/images/new_profile.png",
+                        scale: 2,
+                        fit: BoxFit.fill,
                       ),
-                    ),
-                  ),
+                    ))
+              ],
+            ),
+            SizedBox(
+              height: 60,
+            ),
+            Text(
+              textAlign: TextAlign.center,
+              name,
+              style: TextStyle(color: home1, fontWeight: FontWeight.w700),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(mobNum, style: TextStyle(fontSize: 12),),
+            SizedBox(
+              height: 40,
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  "We're here to help",
+                  style: TextStyle(fontWeight: FontWeight.w700, color: home1, fontSize: 17),
                 ),
-              );
-            }));
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  "Reach out to our support team with your inquiries",
+                  style: TextStyle( color: Colors.grey, fontSize: 13),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 10),
+              child: Divider(),
+            ),
+            SizedBox(height: 20,),
+            SingleChildScrollView(
+             // scrollDirection: Axis.vertical,
+              child: FittedBox(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 10, left: 10),
+                      child: Container(
+                        height: 100,
+                          width: MediaQuery.of(context).size.width * 0.5,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [
+                                BoxShadow(
+                                    color: Colors.black12.withAlpha(20),
+                                    offset: Offset(1, 0),
+                                    blurRadius: 8,
+                                    spreadRadius: 2)
+                              ]),
+                          child:
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+
+                            Container(
+                              decoration: BoxDecoration(
+                                color: home1.withAlpha(40),
+                                borderRadius: BorderRadius.circular(5)
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Icon(
+                                  Icons.call,
+                                  color: home1,
+                                ),
+                              ),
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+        
+                                Text("Contact Us",style: TextStyle(color: Colors.black, fontWeight: FontWeight.w600),),
+                                Text("6AM - 6PM Mon to Fri", maxLines: null,softWrap: true,overflow: TextOverflow.ellipsis,style: TextStyle(fontSize: 10),)
+                              ],
+                            )
+                          ],)
+                        ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 10),
+                      child: Container(
+                          width: MediaQuery.of(context).size.width * 0.5,
+                          height: 100,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [
+                                BoxShadow(
+                                    color: Colors.black12.withAlpha(20),
+                                    offset: Offset(1, 0),
+                                    blurRadius: 8,
+                                    spreadRadius: 2)
+                              ]),
+                          child:    Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                  color: home1.withAlpha(40),
+                                  borderRadius: BorderRadius.circular(5)
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Icon(
+                                    Icons.email,
+                                    color: home1,
+                                  ),
+                                ),
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+
+                                  Text("Email Us",style: TextStyle(color: Colors.black, fontWeight: FontWeight.w600),),
+                                  Text("cards@transcorpint.com", overflow: TextOverflow.ellipsis,style: TextStyle(fontSize: 10),)
+                                ],
+                              )
+                            ],)),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  "Terms & Conditions",
+                  style: TextStyle(fontWeight: FontWeight.w500, color: home1),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 10),
+              child: Divider(),
+            ),
+            SizedBox(height: 10,),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Container(
+                width : double.infinity,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black12.withAlpha(20),
+                          offset: Offset(0, 1),
+                          blurRadius: 8, spreadRadius: 2
+                      )
+                    ]
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(width: 10,),
+                    Container(
+                        decoration: BoxDecoration(
+                          color: home1.withAlpha(40),
+                          borderRadius: BorderRadius.circular(5)
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Icon(Icons.notes,color: home1,),
+                        )),
+                    SizedBox(width: 10,),
+                    Text("English version")
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 20,),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Container(
+                width : double.infinity,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black12.withAlpha(20),
+                          offset: Offset(0, 3),
+                          blurRadius: 9, spreadRadius: 1
+                      )
+                    ]
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(width: 10,),
+                    Container(
+                        decoration: BoxDecoration(
+                          color: home1.withAlpha(40),
+                          borderRadius: BorderRadius.circular(5)
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Icon(Icons.notes,color: home1,),
+                        )),
+                    SizedBox(width: 10,),
+                    Text("Hindi version")
+                  ],
+                ),
+              ),
+            ),
+SizedBox(height: 20,),
+ElevatedButton(onPressed: (){},
+    style: ElevatedButton.styleFrom(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      backgroundColor: home1,
+      foregroundColor: Colors.white
+    ),
+    child: Text("Logout", style: TextStyle(fontWeight: FontWeight.w700),))
+          ],
+        ),
+      ),
+    );
   }
 }
